d7899bf2d256f2139a33ff6644462d70ba307fb0
Pablo Nathanielle Gomez <gomezdevlopment@gmail.com>
2022-09-21 15:39:56 -0400

    Initial commit

---
+++ read-more/src/main/java/com/gomezdevlopment/readmore/ReadMoreApplication.java
@@ -0,0 +1,13 @@
+package com.gomezdevlopment.readmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadMoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadMoreApplication.class, args);
+	}
+
+}
---
+++ read-more/src/test/java/com/gomezdevlopment/readmore/ReadMoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomezdevlopment.readmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadMoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
